5e6cf37ce5390260b046da76c7219ac16f348280
RunningJoke <47787778+RunningJoke@users.noreply.github.com>
2020-08-23 21:59:06 +0000

    Deploying to gh-pages from  @ 4e12ff03fa03c4bf14cc56afefb143895cde2c77 🚀

---
+++ precache-manifest.10980903170918e015111739827f7ba4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b3e5758f65bb9bdafb4",
+    "url": "/css/app.601d2ada.css"
+  },
+  {
+    "revision": "ad23cac830a0bd1541f4",
+    "url": "/css/scoreboard.73a630fb.css"
+  },
+  {
+    "revision": "63defaff6fd1a071182545ee9af93b4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b3e5758f65bb9bdafb4",
+    "url": "/js/app.9514770b.js"
+  },
+  {
+    "revision": "ab03d2032c7a7bfeabb9",
+    "url": "/js/chunk-vendors.724bd317.js"
+  },
+  {
+    "revision": "ad23cac830a0bd1541f4",
+    "url": "/js/scoreboard.82a61c32.js"
+  },
+  {
+    "revision": "6fe49de837bc178a944b8ddc395be100",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
